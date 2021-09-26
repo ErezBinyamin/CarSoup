272e6b176ef67db95645e237a69e16d010fb0196
Erez Binyamin <ezbn2532@gmail.com>
2021-09-26 16:13:46 -0400

    requirements.txt: Added Scrapy to container, run takes --build arg

--- run.sh
+++ run.sh
@@ -2,22 +2,13 @@
 
 CarSoup() {
 	local IMAGE_NAME=carsoup
-	local SRC=app/car.py
-
-	IMG_TIME=0
-	docker image ls | grep -q ${IMAGE_NAME} && IMG_TIME=$( date -d $(docker inspect -f '{{ .Created }}' ${IMAGE_NAME}) +%s)
-	DFILE_TIME=$( date -r Dockerfile +%s )
-	SRC_TIME=$( date -r ${SRC} +%s )
-
-	if [ $DFILE_TIME -gt $IMG_TIME ] 
-	then
-		docker build -t ${IMAGE_NAME} .
-	elif [ $SRC_TIME -gt $IMG_TIME ] && git status -s | grep -iqm1 ${SRC} 
+		
+	if echo "$@" | grep -q '\-\-build'
 	then
 		docker build -t ${IMAGE_NAME} .
 	fi
+	docker run -it ${IMAGE_NAME} python car.py $(echo $@ | grep -v '\-\-build')
 
-	docker run -it ${IMAGE_NAME} python car.py $@
 }
 
 CarSoup $@
